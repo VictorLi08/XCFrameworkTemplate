278fe4d8fc70809d18eebc871bd6087f26aabfc9
Victor Li <victorli08@outlook.com>
2019-07-08 17:29:56 -0700

    Updating for new build system

--- NewSwiftFramework/Build/Intermediates.noindex/NewSwiftFramework.build/Debug-iphoneos/NewSwiftFramework-Universal.build/Script-D52E22D422CAED2200850337.sh
+++ NewSwiftFramework/Build/Intermediates.noindex/NewSwiftFramework.build/Debug-iphoneos/NewSwiftFramework-Universal.build/Script-D52E22D422CAED2200850337.sh
@@ -5,13 +5,13 @@
 rm -rf ${PROJECT_NAME}.xcframework
 
 # Step 2. Build .framework for iOS Device
-xcodebuild -scheme ${PROJECT_NAME} -configuration ${CONFIGURATION} -sdk iphoneos clean build
+xcodebuild -scheme ${PROJECT_NAME} -configuration ${CONFIGURATION} -sdk iphoneos OBJROOT="${OBJROOT}/DependentBuilds"
 
 # Step 3. Build .framework for iOS Simulator
-xcodebuild -scheme ${PROJECT_NAME} -configuration ${CONFIGURATION} -sdk iphonesimulator clean build
+xcodebuild -scheme ${PROJECT_NAME} -configuration ${CONFIGURATION} -sdk iphonesimulator OBJROOT="${OBJROOT}/DependentBuilds"
 
 # Step 4. Build .framework for macOS (optional, only if your framework supports macOS)
-xcodebuild -scheme "${PROJECT_NAME}-macOS" -configuration ${CONFIGURATION} clean build
+xcodebuild -scheme "${PROJECT_NAME}-macOS" -configuration ${CONFIGURATION} OBJROOT="${OBJROOT}/DependentBuilds"
 
 # Step 5. Create .xcframework using each of the .frameworks built previously 
 xcodebuild -create-xcframework -framework "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework" -framework "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PROJECT_NAME}.framework" -framework "${BUILD_DIR}/${CONFIGURATION}/${PROJECT_NAME}_macOS.framework" -output ${PROJECT_NAME}.xcframework 
